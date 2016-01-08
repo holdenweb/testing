01da881f4f5ea7db15d207d5f3baa6ac30fd1879
Steve Holden <steve@holdenweb.com>
2016-01-08 00:35:32 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from disutils.core import setup
+
+setup(name='testing',
+      version='1.0',
+      description='My bery own test package',
+      author='Steve Holden',
+      author_email='steve@holdenweb.com',
+      url='http://holdenweb.com',
+      packages=['testing'],
+     )
